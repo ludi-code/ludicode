a4b7561958e8162b76545b6d99434dfc7ac3bda8
Marie Vitse <marie_vts@outlook.fr>
2017-03-24 17:10:48 +0100

    début lien client/serv enregistrement new user

--- src/main/webapp/scripts/page_controllers/register.js
+++ src/main/webapp/scripts/page_controllers/register.js
@@ -2,7 +2,7 @@
  * Handle the creation of a new user in the base
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	$("#error").hide();
 
@@ -10,9 +10,13 @@ $(document).ready(function() {
 	$("#button_register").click(function() {
 		var name = $("#name_register").val();
 		var passwd = $("#password_register").val();
-		var email = $("#email_register").val();
-
-		registerUser(name, passwd, email);
+        
+        if(document.getElementById('checkbox').checked == true) {
+            console.log("checked !");
+            var email = $("#email_register").val();
+            registerUser(name, passwd, email);
+        } else 
+		    registerUser(name, passwd);
 
 		$("#name_register").val("");
 		$("#password_register").val("");
@@ -21,17 +25,54 @@ $(document).ready(function() {
 
 });
 
-function registerUser(name, password, email) {
+function gBox(nbCheck){
+    if(document.getElementById('checkbox').checked == true){
+        //la case est cochée
+        $(".form-mail").show();                               
+    } else{
+       //La case n'est pas cochée
+        $(".form-mail").hide();
+    }
+}
+
+function registerUser(name, password) {
 	$.ajax({
 		type : 'POST',
 		contentType : 'application/json',
-		url : "v1/users/register",
+		url : "v2/students/register",
 		dataType : "json",
 		data : JSON.stringify({
 			"id" : 0,
 			"name" : name,
-			"password" : password, 
-			"email" : email
+			"password" : password
+		}),
+		success : function(data, textStatus, jqXHR) {
+			console.log(data);
+			if(data.success) {
+				loginUser(name, password, "/");	
+			} else {
+				$("#error").empty();
+				$("#error").append(data.message);
+				$("#error").show();
+			}
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('Erreur lors de l\'enregistrement de votre compte : ' + textStatus);
+		}
+	});
+}
+
+function registerUser(name, password, email) {
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : "v2/teachers/register",
+		dataType : "json",
+		data : JSON.stringify({
+			"id" : 0,
+			"name" : name,
+			"password" : password,
+            "email" : email
 		}),
 		success : function(data, textStatus, jqXHR) {
 			console.log(data);
