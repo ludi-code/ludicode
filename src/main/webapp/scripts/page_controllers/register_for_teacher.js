4cd33c6bfa56d4f05b53fbc495fecc6eda1405ca
Devassia <a.devassine@orange.fr>
2017-03-28 09:44:28 +0200

    groupes et register modif finale

--- src/main/webapp/scripts/page_controllers/register_for_teacher.js
+++ src/main/webapp/scripts/page_controllers/register_for_teacher.js
@@ -8,22 +8,42 @@ $("input[type='checkbox']").click(function(){
         $(".boutons").hide();
     }
 })
-
-function form(nbCheck){
-    if(document.getElementById('checkbox').checked == true){
-        //la case est cochée
-        $(".form-add").show();                               
-    } else{
-       //La case n'est pas cochée
-        $(".form-add").hide();
-    }
+//Permet d'enregistrer un eleve 
+ function registerStudent(name, password) {
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : "v2/students/register",
+		dataType : "json",
+		data : JSON.stringify({
+			//"id" : 0,
+			"name" : name,
+			"password" : password
+		}),
+		success : function(data, textStatus, jqXHR) {
+			console.log(data);
+			if(data.success) {
+				loginUser(name, password, "/");	
+			} else {
+				$("#error").empty();
+				$("#error").append(data.message);
+				$("#error").show();
+			}
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('Erreur lors de l\'enregistrement de votre compte : ' + textStatus);
+		}
+	});
 }
+
  //Gestion du bouton Ajouter
- 
-
-
-
-
+/*function gButton(nbCheck){
+    /*if(document.getElementById('bouton0').checked == true){
+    	console.log("bonjour");
+        $(".form-mail").show();                               
+    } */
+   // console.log("test");
+//}
 
 });
 
