d5fa1e26ef257219bf58b3f32bb18868ca9f043c
François Morel <francois.morel.sfe@gmail.com>
2017-03-28 09:59:27 +0200

    Réapparition du fichier Comment ca marche

--- src/main/java/fr/iutinfo/beans/ParseLevel.java
+++ src/main/java/fr/iutinfo/beans/ParseLevel.java
@@ -8,36 +8,65 @@ import java.io.InputStreamReader;
 
 /*
  * Permet de créer un niveau à partir d'un fichier envoyé par un enseignant
- * Permmettra à celui-ci de créer des niveaux rapidement
+ * Permettra à celui-ci de créer des niveaux rapidement
  * Attention : format du fichier extrémement précis !
- * @author François Morel
+ * @author François
  */
 public class ParseLevel{
 	
 	public static Level parseText(String file){
+		boolean nonConforme=false;
 		Level level = new Level();
 		try{
 			InputStream ips=new FileInputStream(new File(file)); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
 			String ligne;
-			while((ligne=br.readLine())!=null){
+			while((ligne=br.readLine())!=null && nonConforme==false){
 				if(ligne.charAt(0)!='#'){
 					if(ligne.length()>5 && ligne.substring(0,5).equals("Nom: ")){
 						String nom=ligne.substring(5,ligne.length());
 						level.setName(nom);
 					}
-					if(ligne.length()>13 && ligne.substring(0,13).equals("Orientation: ")){
-						String orientation=ligne.substring(13,ligne.length());
-						if(orientation.equals("true")){
+					if(ligne.length()>6 && ligne.substring(0,6).equals("Type: ")){
+						String type=ligne.substring(6,ligne.length()).toLowerCase();
+						if(type.equals("blocks")||type.equals("block")){
 							level.setOrientation(true);
+							level.setlevelType("blocks");
 						}else{
-							level.setOrientation(false);
+							if(type.equals("cartes")||type.equals("carte")){
+								level.setlevelType("cartes");
+							}else if(type.equals("python")){
+								level.setlevelType(type);
+							}else{
+								nonConforme=true;
+							}
+							if(ligne.length()>13 && ligne.substring(0,13).equals("Orientation: ")){
+								String orientation=ligne.substring(13,ligne.length()).toLowerCase();
+								if(orientation.equals("absoluex")||orientation.equals("absolu")){
+									level.setOrientation(false);
+								}else if(orientation.equals("relative")||orientation.equals("relatif")){
+									level.setOrientation(true);
+								}else{
+									nonConforme=true;
+								}
+							}
 						}
+					}
+					if(ligne.length()>18 && ligne.substring(0,18).equals("Max_instructions: ")){
+						String max_instru=ligne.substring(18,ligne.length());
+						level.setMaxInstructions(Integer.parseInt(max_instru));
+					}
+					if(ligne.length()>18 && ligne.substring(0,18).equals("Max_instructions: ")){
+						String max_instru=ligne.substring(18,ligne.length());
+						level.setMaxInstructions(Integer.parseInt(max_instru));
 					}
 				}
 			}
-			br.close(); 
+			br.close();
+			if(nonConforme==true){
+				System.out.println("ATTENTION: votre fichier n'est pas à un format conforme! Prenez exemple sur le fichier type!");
+			}
 		}catch (Exception e){
 			System.out.println(e.toString());
 		}
