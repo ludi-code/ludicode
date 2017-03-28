523b7cfda0ed1ea580b4015996e62d6a9ff3fa33
François Morel <francois.morel.sfe@gmail.com>
2017-03-28 08:29:49 +0200

    Continuation de la classe parseLevel

--- src/main/java/fr/iutinfo/beans/ParseLevel.java
+++ src/main/java/fr/iutinfo/beans/ParseLevel.java
@@ -6,26 +6,46 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class ParseLevel {
+/*
+ * Permet de créer un niveau à partir d'un fichier envoyé par un enseignant
+ * Permmettra à celui-ci de créer des niveaux rapidement
+ * Attention : format du fichier extrémement précis !
+ * @author François Morel
+ */
+public class ParseLevel{
 	
-	public Level level;
-	
-	public static void parseText(String file){
+	public static Level parseText(String file){
+		Level level = new Level();
 		try{
 			InputStream ips=new FileInputStream(new File(file)); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
 			String ligne;
 			while((ligne=br.readLine())!=null){
-				System.out.println(ligne);
+				if(ligne.charAt(0)!='#'){
+					if(ligne.length()>5 && ligne.substring(0,5).equals("Nom: ")){
+						String nom=ligne.substring(5,ligne.length());
+						level.setName(nom);
+					}
+					if(ligne.length()>13 && ligne.substring(0,13).equals("Orientation: ")){
+						String orientation=ligne.substring(13,ligne.length());
+						if(orientation.equals("true")){
+							level.setOrientation(true);
+						}else{
+							level.setOrientation(false);
+						}
+					}
+				}
 			}
 			br.close(); 
 		}catch (Exception e){
 			System.out.println(e.toString());
 		}
+		System.out.println(level);
+		return level;
 	}
 	
 	public static void main(String[] args){
-		parseText("/home/infoetu/morelf/Agile/ludicode/Oui.txt");
+		parseText("/home/infoetu/morelf/Agile/ludicode/Test.txt");
 	}
 }
--- src/main/webapp/scripts/page_controllers/index.js
+++ src/main/webapp/scripts/page_controllers/index.js
@@ -1,3 +1,6 @@
+//Permet de récupérer des informations tel que le nombre d'éléves, d'enseignants ou de niveaux existants
+//grâce à la classe CountResource.java pour la page index.html
+
 function getNumTeach(){
 	 $.getJSON("/v2/count/countResource/", function (data) {
          $('#nbTeachers').append(data.numTeachers + " enseignants motivés!");
