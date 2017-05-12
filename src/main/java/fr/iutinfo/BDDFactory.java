6f91cc6b0894dc6e85534a3c9c04d50de9a1054d
Thomas Clavier <tom@tcweb.org>
2017-05-12 21:49:12 +0200

    Refactor, BDDFactory : move db file

--- src/main/java/fr/iutinfo/BDDFactory.java
+++ src/main/java/fr/iutinfo/BDDFactory.java
@@ -1,19 +1,24 @@
 package fr.iutinfo;
 
 import org.skife.jdbi.v2.DBI;
+import org.slf4j.LoggerFactory;
 import org.sqlite.SQLiteDataSource;
+import org.slf4j.Logger;
 
 import javax.inject.Singleton;
 
 @Singleton
 public class BDDFactory {
+    final static Logger logger = LoggerFactory.getLogger(BDDFactory.class);
     private static DBI dbi = null;
 
     public static DBI getDbi() {
         if (dbi == null) {
             SQLiteDataSource ds = new SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:" + System.getProperty("user.home") + System.getProperty("file.separator") + "ludicode.db");
+            String dbFile = System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") + "ludicode.db";
+            ds.setUrl("jdbc:sqlite:" + dbFile);
             dbi = new DBI(ds);
+            logger.debug("database file : " + dbFile);
         }
         return dbi;
     }
